5eabea6caa1aadb28591f615ff692b596ffbcac0
Chris Huys <chrishuys@live.com>
2017-02-21 16:03:22 +0100

    bal

--- WensKaart/WensKaart/Kleur.cs
+++ WensKaart/WensKaart/Kleur.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace WensKaart
 {
     public class Kleur
     {
+        public SolidColorBrush Borstel { get; set; }
+        public string Naam { get; set; }
+        public string Hex { get; set; }
+        public byte Rood { get; set; }
+        public byte Groen { get; set; }
+        public byte Blauw { get; set; }
     }
 }
--- WensKaart/WensKaart/WensKaartWindow.xaml.cs
+++ WensKaart/WensKaart/WensKaartWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,7 +28,7 @@ namespace WensKaart
         {
             InitializeComponent();
 
-            foreach (StylusPointPropertyInfo info in typeof(Colors).GetProperties())
+            foreach (PropertyInfo info in typeof(Colors).GetProperties())
             {
                 BrushConverter bc = new BrushConverter();
                 SolidColorBrush deKleur =
@@ -36,32 +40,20 @@ namespace WensKaart
                 kleurke.Rood = deKleur.Color.R;
                 kleurke.Groen = deKleur.Color.G;
                 kleurke.Blauw = deKleur.Color.B;
-                cirkelsKleuren.Items.Add(kleurke);
-                rechthoekenKleuren.Items.Add(kleurke);
-                cirkelKaderKleuren.Items.Add(kleurke);
-                rechthoekKaderKleuren.Items.Add(kleurke);
+                balKleuren.Items.Add(kleurke);
+              
             }
-            BrushConverter bc = new BrushConverter();
-            SolidColorBrush deKleur =
-(SolidColorBrush)bc.ConvertFromString(info.Name);
-            Kleur kleurke = new Kleur();
-            kleurke.Borstel = deKleur;
-            kleurke.Naam = info.Name;
-            kleurke.Hex = deKleur.ToString();
-            kleurke.Rood = deKleur.Color.R;
-            kleurke.Groen = deKleur.Color.G;
-            kleurke.Blauw = deKleur.Color.B;
-            cirkelsKleuren.Items.Add(kleurke);
-            rechthoekenKleuren.Items.Add(kleurke);
-            cirkelKaderKleuren.Items.Add(kleurke);
-            rechthoekKaderKleuren.Items.Add(kleurke);
+ 
         }
 
         private void meer_Click(object sender, RoutedEventArgs e)
         {
             int aantal = Convert.ToInt16(grootteText.Content);
             if (aantal < 40)
-            { aantal++; }
+            {
+                aantal++;
+                Wens.FontSize++;
+            }
             grootteText.Content = aantal.ToString();
         }
 
@@ -69,8 +61,88 @@ namespace WensKaart
         {
             int aantal = Convert.ToInt16(grootteText.Content);
             if (aantal > 10)
-            { aantal--; }
+            {
+                aantal--;
+                Wens.FontSize--;
+            }
             grootteText.Content = aantal.ToString();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            lettertypesCombobox.Items.SortDescriptions.Add(new SortDescription("Source", ListSortDirection.Ascending));
+            lettertypesCombobox.SelectedItem = new FontFamily(Wens.FontFamily.ToString());
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Filter = "Wenskaarten | *.wens";
+                dlg.FileName = "Wenskaart";
+                dlg.DefaultExt = ".wens";
+
+                if (dlg.ShowDialog() == true)
+                {
+                    using (StreamWriter bestand = new StreamWriter(dlg.FileName))
+                    {
+                        MessageBox.Show(Wens.Text.ToString());
+                        bestand.WriteLine(Wens.Text.ToString());
+                        bestand.WriteLine(Wens.SelectedValue.ToString());
+                        bestand.WriteLine(Wens.FontSize.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Er ging iets fout bij het saven" + ex.Message, "Foutmelding", MessageBoxButton.OK);
+            }
+
+        }
+
+        private void NewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Nieuw();
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+           
+            try
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Filter = "Wenskaarten | *.wens";
+
+                if (dlg.ShowDialog() == true)
+                {
+                    using (StreamReader bestand = new StreamReader(dlg.FileName))
+                    {
+                        Wens.Text = bestand.ReadLine();
+                        Wens.FontFamily = new FontFamily(bestand.ReadLine());
+                     //   Wens.FontSize = (FontSize)ConvertFromString(bestand.ReadLine());
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            
+        }
+        private void PrintPreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void Nieuw()
+        {
+            StatusItem.Content = "Nieuw";
         }
     }
 }
